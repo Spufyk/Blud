d82525d07f19e24099e81124f53cb81df59bc569
J <jgrs.seven@gmail.com>
2024-10-09 09:39:10 -0300

    fotos

---
+++ blud/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace blud;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ blud/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace blud;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
